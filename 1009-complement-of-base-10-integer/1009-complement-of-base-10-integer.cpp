663e00aa0e3ca49c04e60eb1a00084c42a11cf4d
GAURAV-SSD <gauravpalwal0052@gmail.com>
2026-09-12 23:23:56 +0530

    Time: 0 ms (100%), Space: 7.8 MB (92.19%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m=n;
+        int mask=0;
+        if(n==0){
+            return 1;
+        }
+        
+        while(m!=0){
+            mask=(mask<<1) | 1;
+            m=m>>1;
+
+
+        }
+        int ans=(~n) & mask;
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
