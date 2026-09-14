5b8f9efebfc3ad0b2b3eed5f1510906106ce9707
GAURAV-SSD <gauravpalwal0052@gmail.com>
2026-09-14 23:52:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (45.52%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -2,12 +2,20 @@ class Solution {
 public:
     int addDigits(int num) {
         
-        if (num == 0) return 0;
-        if (num % 9 == 0) return 9;
-        return num % 9;
+     if (num<=0) return 0;
+    while (num>=10){
+        int sum=0;
+        while (num>0){
+            int digit = num%10;
+            num/=10;
+            sum+=digit;
+        }
+        num = sum;
     }
-
+    return num;
+    }
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
